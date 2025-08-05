d84ac660e332cfd1d056514bc027fc5119abaa9c
leefrank9527 <leefrank9527@gmail.com>
2025-08-05 22:29:00 +1200

    fixed the deb  build issues

--- src/main/java/org/webcurator/visualization/springboot/MainApp.java
+++ src/main/java/org/webcurator/visualization/springboot/MainApp.java
@@ -7,12 +7,16 @@ import javafx.scene.web.WebView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 
 public class MainApp extends Application {
+    private final WebView webView = new WebView();
+    private final ProgressIndicator spinner = new ProgressIndicator();
+
     public static void main(String[] args) {
         startRestApiServer();
         waitForServer(8080, 10000); // wait max 10 seconds
@@ -21,20 +25,36 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws MalformedURLException {
-        WebView webView = new WebView();
-        // URL url = getClass().getResource("/index.html");
-        // if (url == null) {
-        // System.err.println("index.html not found");
-        // System.exit(1);
-        // }
+        BorderPane root = new BorderPane();
 
-        URL url = new URL("http://localhost:8080");
-        webView.getEngine().load(url.toExternalForm());
+        // Top toolbar
+        ToolBar toolBar = new ToolBar();
+        Button selectBtn = new Button("Select File/Folder");
+        // toolBar.getItems().add(selectBtn);
+        Label pathLabel = new Label("No selection");
+        pathLabel.setMaxWidth(Double.MAX_VALUE);
+        toolBar.getItems().addAll(selectBtn, new Separator(), pathLabel);
+        root.setTop(toolBar);
 
-        stage.setTitle("WAVA");
-        stage.setScene(new Scene(webView));
-        // stage.setFullScreen(true);
-        // stage.show();
+        StackPane centerPane = new StackPane();
+        centerPane.getChildren().addAll(webView, spinner);
+        spinner.setVisible(false);
+        webView.setVisible(false);
+        root.setCenter(centerPane);
+
+        // File chooser action
+        selectBtn.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            File selected = fileChooser.showOpenDialog(stage);
+            if (selected != null) {
+                pathLabel.setText(selected.getAbsolutePath());
+                processFile(selected);
+            }
+        });
+
+        Scene scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         stage.setX(screenBounds.getMinX());
         stage.setY(screenBounds.getMinY());
